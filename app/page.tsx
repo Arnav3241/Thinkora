9ce24a0ae0ba80234bf551134bbdc1a32cf47bc3
Arnav Singh <arnav.singh.legendary.202@gmail.com>
2024-09-13 20:44:29 +0530

    Basic Structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import Calendar from "./../components/calendar";
+
 export default function Home() {
   return (
-    <div className="text-8xl font-semibold p-10">Thinkora - Under Development</div>
+    <div className="relative h-screen flex items-center justify-center bg-[#e2e1dc]">
+      <div className="relative bg-[#373c4f] min-w-[1300px] min-h-[780px] rounded-[10px] flex">
+        <div className="left w-[60%] p-[20px] min-h-[780px]">
+          <Calendar />
+        </div>
+        <div className="right w-[40%] p-[20px] min-h-[780px]">
+          RIght
+        </div>
+      </div>
+    </div>
   );
 }
---
+++ components/calendar.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-red-500 h-full bg-slate-300">
+      Calendar Element 
+    </div>
+  );
+};
